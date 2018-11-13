ce8c8e5018d80d221bfbfadc436137bc1057dd0d
Ragura <ragura@gmail.com>
2018-11-13 18:26:26 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="toonBoodschap">Open modal</button>
+    <cvo-modal :open="isOpen" :titel="titel" :boodschap="boodschap" @close="isOpen = false"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CvoModal from './components/CvoModal.vue'
 
 export default {
   name: 'app',
+  data() {
+    return {
+      isOpen: false,
+      titel: "",
+      boodschap: ""
+    }
+  },
   components: {
-    HelloWorld
-  }
+    CvoModal
+  },
+  methods: {
+    toonBoodschap() {
+      this.isOpen = true;
+      this.titel = "Welkom";
+      this.boodschap = "Deze modal is een herbruikbare component";
+    }
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/CvoModal.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="modal" v-show="open" @click="$emit('close')">
+        <div class="modal-box" @click.stop>
+            <h1>{{ titel }}</h1>
+            <hr>
+            <p>{{ boodschap }}</p>
+            <button @click="$emit('close')">Sluiten</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            open: Boolean,
+            titel: String,
+            boodschap: String
+        },
+    }
+</script>
+
+<style scoped>
+    .modal {
+        display: flex;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        overflow: auto;
+        z-index: 1;
+        background-color: rgba(0,0,0,0.4);
+        justify-content: center;
+        align-items: center;
+    }
+
+    .modal-box {
+        width: 400px;
+        padding: 2em;
+        z-index: 2;
+        background-color: lemonchiffon;
+        text-align: center;
+    }
+
+    
+</style>
